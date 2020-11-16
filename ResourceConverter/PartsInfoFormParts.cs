e7dc35edf492b219ecc41039ad5f05da12717e43
Takayuki Miyanaga <kotatuinu@nifty.com>
2020-11-16 23:53:13 +0900

    EDITTEXTの抽出結果に誤りがあったので、正規表現を見直し。

--- ResourceConverter/PartsInfoFormParts.cs
+++ ResourceConverter/PartsInfoFormParts.cs
@@ -11,7 +11,7 @@ namespace ResourceConverter
             currentPartsInfos_ = new List<PARTS_INFO> {
                 new PARTS_INFO(PARTSKIND.PUSHBUTTON, @"^\s*((?:DEF)*PUSHBUTTON)\s+", @"^\s*((?:DEF)*PUSHBUTTON)\s+""(.*?)"",(.+?),(\d+),(\d+),(\d+),(\d+)(?:,(?:(\w+)(?:\s+\|\s+))*(\w+\s+\w+|\w+)*)*$", RESOURCE_STATUE.FORMPARTS),
                 new PARTS_INFO(PARTSKIND.CONTROL, @"^\s*(CONTROL)\s+", @"^\s*(CONTROL)\s+""(.*?)"",(.+?),""(.+?)"",(?:(\w+\s+\w+|\w+)(?:\s+\|\s+))*(\w+\s+\w+|\w+),(\d+),(\d+),(\d+)(?:,(\d+))*(?:,*(?:(\w+\s+\w+|\w+)(?:\s+\|\s+))*(\w+\s+\w+|\w+)*)*$", RESOURCE_STATUE.FORMPARTS),
-                new PARTS_INFO(PARTSKIND.EDITTEXT, @"^\s*(EDITTEXT)\s+", @"^\s*(EDITTEXT)\s+(.+?),\s*(\d+),\s*(\d+),\s*(\d+),\s*(\d+)(?:,(\d+|\w+)\s*)*(?:,*(\w+\s*\w+)(?:\s*\|\s*)*)*$", RESOURCE_STATUE.FORMPARTS),
+                new PARTS_INFO(PARTSKIND.EDITTEXT, @"^\s*(EDITTEXT)\s+", @"^\s*(EDITTEXT)\s+(.+?),\s*(\d+),\s*(\d+),\s*(\d+),\s*(\d+)(?:,*(?:(\d+|\w+\s*\w+)(?:\s\|\s)*))*$", RESOURCE_STATUE.FORMPARTS),
                 new PARTS_INFO(PARTSKIND.COMBOBOX,@"^\s*COMBOBOX\s+", @"^\s*(COMBOBOX)\s+(.+?),\s*(\d+),\s*(\d+),\s*(\d+),\s*(\d+)(?:,*(?:(\d+|\w+\s*\w+)(?:\s\|\s)*))*$", RESOURCE_STATUE.FORMPARTS),
                 new PARTS_INFO(PARTSKIND.CHECKBOX, @"^\s*CHECKBOX\s+", @"^\s*(CHECKBOX)\s+""(.+?)"",\s*(.+?),\s*(\d+),\s*(\d+),\s*(\d+),\s*(\d+)$", RESOURCE_STATUE.FORMPARTS),
                 new PARTS_INFO(PARTSKIND.ICON, @"^\s*ICON\s+", @"^\s*(ICON)\s+(.+?),\s*(.+?),\s*(\d+),\s*(\d+),\s*(\d+),\s*(\d+)$", RESOURCE_STATUE.FORMPARTS),
